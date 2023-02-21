df94686e35bb978c15245ff276babcde1cdc4c90
alefra88 <efra.rlz@gmail.com>
2023-02-21 02:52:58 -0600

    consumiendo la api de rick y morty con XMLHttpRquest

---
+++ app.js
@@ -0,0 +1,28 @@
+// anon function auto ex
+(() => {
+  const xhr = new XMLHttpRequest(),
+    $xhr = document.getElementById("xhr"),
+    $fragment = document.createDocumentFragment();
+
+  console.log(xhr);
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) return;
+    if (xhr.status >= 200 && xhr.status < 300) {
+      console.log("èxito");
+      let data = ($xhr.innerHTML = xhr.responseText);//hice esta variable para renderizar en el html
+      let json = xhr.responseText;
+      // console.log(json);
+      json.forEach((element) => {//no entiendo porque me tira error de funcion
+        const $li = document.createElement("li");
+        $li.innerHTML = `${element.name}`;
+        $fragment.appendChild("$li");
+      });
+      $xhr.appendChild($fragment);
+      body.appendChild($xhr);
+    } else {
+      console.log("error");
+    }
+  });
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+  xhr.send();
+})();
